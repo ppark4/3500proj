b414969611f9db2844286e64f6904ee9c6f692e1
ppark4 <patparkk@gmail.com>
2022-04-12 15:39:58 -0700

    program can read file, returns semi-clean formatted data

--- ClassProjectGroup2.py
+++ ClassProjectGroup2.py
@@ -1,1 +1,23 @@
-#python code
+# Course: CMPS 3500
+# Class Project
+# Python Implementation: Basic Data Analysis
+# Date: 4/12/2022
+# Student 1: Patrick Park
+# Student 2: Carlos Hernandez
+# Student 3: Edmund Felicidario
+# Student 4: Juan Sierra Diaz
+
+# imports pandas, a useful data science library
+import pandas as pd
+
+# reads file
+data = pd.read_csv("test_data.csv", index_col=0)
+
+# removes rows with specific missing data
+validData = data.dropna(subset=['ID', 'Severity', 'Zipcode','Start_Time','End_Time','Visibility(mi)', 'Weather_Condition','Country'])
+
+# resets index numbers
+validData.reset_index(drop=True,inplace=True)
+
+# prints all rows after cleanup
+print(validData)
--- ClassProjectGroup2.rb
+++ ClassProjectGroup2.rb
@@ -1,1 +1,8 @@
-#ruby code
+# Course: CMPS 3500
+# Class Project
+# Ruby Implementation: Basic Data Analysis
+# Date: 4/12/2022
+# Student 1: Patrick Park
+# Student 2: Carlos Hernandez
+# Student 3: Edmund Felicidario
+# Student 4: Juan Sierra Diaz
