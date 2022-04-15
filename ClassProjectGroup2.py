31041977e445a15ffa947b975c1b86830decef17
Carlos Hernandez <Carlosh412@outlook.com>
2022-04-14 21:50:59 -0700

    drop rows with 3 or more missing columns,distance equal to zero & set up elapsed time

--- ClassProjectGroup2.py
+++ ClassProjectGroup2.py
@@ -8,16 +8,29 @@
 # Student 4: Juan Sierra Diaz
 
 # imports pandas, a useful data science library
+import csv
 import pandas as pd
+import numpy as np
+from datetime import date
+
 
 # reads file
 data = pd.read_csv("test_data.csv", index_col=0)
-
 # removes rows with specific missing data
 validData = data.dropna(subset=['ID', 'Severity', 'Zipcode','Start_Time','End_Time','Visibility(mi)', 'Weather_Condition','Country'])
 
+#delete rows with 0 in distance 
+data.shape
+validData = data.loc[data['Distance(mi)'] != 0]
+data.shape
+#drop rows with 3 or more missing column values
+validData = validData.dropna(axis=0,thresh=18)
+#remove rows with elapsed time equal to zero
+delta = pd.to_datetime(data["End_Time"])-pd.to_datetime(data["Start_Time"])
 # resets index numbers
 validData.reset_index(drop=True,inplace=True)
 
 # prints all rows after cleanup
 print(validData)
+
+print(delta)
