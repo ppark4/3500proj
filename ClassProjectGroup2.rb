74d931b36d041fce580efad7af4d1b46106454b7
Pablo <juansiediaz97@gmail.com>
2022-04-18 10:13:06 -0700

    Load test data in datafram

--- ClassProjectGroup2.rb
+++ ClassProjectGroup2.rb
@@ -6,3 +6,12 @@
 # Student 2: Carlos Hernandez
 # Student 3: Edmund Felicidario
 # Student 4: Juan Sierra Diaz
+
+#!/usr/bin/env ruby
+require 'daru'
+require 'csv'
+
+
+
+df = Daru::DataFrame.from_csv('test_data.csv')
+puts df
